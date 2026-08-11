6df03fce0abc30d546b09934ec02cf3f9f6b2eb9
= <pierre.aurel@proton.me>
2026-08-11 17:38:33 +0200

    update basic unit test

--- tests/basic.nix
+++ tests/basic.nix
@@ -7,25 +7,30 @@
       self.nixosModules.daedalus
     ];
 
-    users.users.test = {
-      isNormalUser = true;
-      password = "test";
-    };
-
     virtualisation.memorySize = 2048;
   };
 
   testScript = ''
     machine.start()
 
+    # 1. The system actually boots.
     machine.wait_for_unit("multi-user.target")
 
+    # 2. Systemd considers the system healthy.
     machine.succeed("systemctl is-system-running --wait")
 
-    machine.succeed("id test")
+    # 3. The default user exits.
+    machine.succeed("id daedalus")
 
+    # 4. Networking is functionnal.
     machine.succeed("systemctl is-active NetworkManager")
+    machine.succeed("ip link")
 
-    machine.succeed("systemctl is-active polkit")
+    # 5. Basic Nix functionality works.
+    machine.succeed("nix --version")
+
+    # 6. The expected hostname is applied.
+    machine.succeed("hostname")
+    machine.succeed("test \"$(hostname)\" = \"labyrinth\"")
   '';
 }
